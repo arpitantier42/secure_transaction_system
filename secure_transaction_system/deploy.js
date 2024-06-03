7036adf69d24a6d5ba15b1874b941dc3df011e22
arpitk777 <arpit.khandelwal@antiersolutions.com>
2024-06-03 15:08:27 +0530

    deployment script for wasm contract

--- secure_transaction_system/deploy.js
+++ secure_transaction_system/deploy.js
@@ -1,65 +1,41 @@
 import { CodePromise, Abi, ContractPromise } from '@polkadot/api-contract';
 import { ApiPromise, WsProvider, Keyring} from '@polkadot/api';
+import { BN, BN_ONE, BN_ZERO } from "@polkadot/util";
 
-// import .contract file as json string
+
 import { json } from "./abi.js"
+ 
+  // const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+  const wsProvider = new WsProvider('wss://wss-testnet-nodes.saitascan.io');
 
-
-try {
-  let address; // variable for storing the address of the deployed contract 
-
-  // API creation for connection to the chain
-  const wsProvider = new WsProvider('wss://wss-testnet.5ire.network/');
-  const api = await ApiPromise.create({ provider: wsProvider });
-
-  // convert json into usable contract ABI 
-  let contractAbi = new Abi(json, api?.registry?.getChainProperties());
+  const api = await ApiPromise.create({ provider: wsProvider });  
 
   // instantiating wasm blob on the blockchain
   const code = new CodePromise(api, json, json.source.wasm);
-  
-  // gas limit for deployment
-  const gasLimit = 100000n * 1000000n
 
-  // endowment
-  const value = 0;
+  const storageDepositLimit = null;
   
-  // adding fire account for paying the gas fee
-  const PHRASE = 'negative cheap cherry uncover absurd angle swarm armor tuna lounge hurdle lawsuit';
-  const keyring = new Keyring({ type: "ed25519" });
-  const userKeyring = keyring.addFromMnemonic(PHRASE);
-  // parameters for constructor function inside the contract
+  const value = '0'
+  
+  const keyring = new Keyring({ type: "ethereum" });
+  
+  const userKeyring = keyring.addFromUri('0x330a99a914a315909e44b3a5da723798c2010f3720608db7c18b6f32ac86f858');
 
-  // Constructor New
   let constructorIndex = 0;
 
-  try {
+  const gasLimit = api.registry.createType("WeightV2", {
+    refTime: new BN("10000000000"),
+    proofSize: new BN("10000000000"),
+  });
+ 
+  const tx = code.tx['new']({ value:0, gasLimit:gasLimit , storageDepositLimit }, userKeyring.publicKey);
 
-    // upload wasm blob
-    let newMethod = code && contractAbi?.constructors[constructorIndex]?.method
-      ? code.tx[contractAbi.constructors[constructorIndex].method](
-        userKeyring.address,
-        {
-        gasLimit: gasLimit,
-        storageDepositLimit: null,
-        value: value
-      })
-    : null;
-
-    // code deploy
-    const unsub = await newMethod.signAndSend(userKeyring, async (response) => {
-      if (response.status.isInBlock || response.status.isFinalized) {
-        address = response.contract.address.toString();
-        console.log("address ====== ", address);
-        unsub();
-      }
-    });
-
-} catch (e) {
-    console.log("error catch", e);
-}
-
-}
-catch(err){
-  console.log("error",err.toString())
-}
+  const unsub = await tx.signAndSend(userKeyring, {signer: userKeyring}, ({contract, status, events}) => {
+    console.log('status', status.toHuman())
+    if(contract) {
+      const addr = events.filter(e => e.event.method == 'Instantiated')[0].event.data.toHuman().contract;
+      console.log('Contract address: ', addr)
+      
+      unsub()
+    }
+  })
